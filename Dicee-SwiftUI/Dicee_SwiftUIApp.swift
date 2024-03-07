2d00f7914f09fca8909c656ff44691a32fbea0c2
VladVorniceanu <vorniceanu.v@gmail.com>
2024-03-07 14:19:02 +0200

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Vlad Vorniceanu on 07.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
